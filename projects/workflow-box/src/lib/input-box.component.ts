ccdca9120a516029b2a59e16a8c1a9e15a645307
luca.vescovi <luca.vescovi@accenture.com>
2023-04-11 20:31:26 +0200

    add page with lib

--- projects/workflow-box/src/lib/input-box.component.ts
+++ projects/workflow-box/src/lib/input-box.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-inputBox',
+  selector: 'form-input-box',
   template: `
     <p>
       input-box works!
     </p>
 
     <p>
-      !!inputLists ? !!inputLists[0] : 'lista vuota'
+      {{!!inputLists && !!inputLists[0] ? inputLists[0].value : 'Lista vuota'}}
     </p>
   `,
   styles: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,32 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeAngularComponent } from 'projects/home-angular/src/public-api';
 import { PageNotFoundComponent } from 'projects/page-not-found/src/public-api';
+import { PageComponent } from './page/page.component';
+import { InputBoxModule } from "../../projects/workflow-box/src/lib/input-box.module";
 
 
 const routes: Routes = [
   {path:'', redirectTo:'/page',  pathMatch: 'full'},
   { path: 'home-angular', component: HomeAngularComponent,   pathMatch: 'full' },
-  { path: 'page', component: HomeAngularComponent },
+  { path: 'page', component: PageComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule
-  ],
-  exports:[
-    RouterModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PageComponent
+    ],
+    exports: [
+        RouterModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        InputBoxModule
+    ]
 })
 export class AppModule { }
+
